8da7d6fd9fdb8b0451589894c0df108ec98e6318
[Abdu-rahman Abed] <[hanini1999@gmail.com]>
2018-09-09 19:37:08 -0400

    MakePyramid

--- src/math/problems/MakePyramid.java
+++ src/math/problems/MakePyramid.java
@@ -2,18 +2,14 @@ package math.problems;
 
 public class MakePyramid {
     public static void main(String[] args) {
+        for (int i=1; i<10; i += 2) {
+            for (int j=0; j < (4 - i / 2); j++) {
+                System.out.print(" ");
+            } for (int t=0; t<i; t++) {
+                System.out.print("*");
+            }
 
-        /*   Implement a large Pyramid of stars in the screen with java.
-
-                              *
-                             * *
-                            * * *
-                           * * * *
-                          * * * * *
-                         * * * * * *
-
-        */
-
-
+            System.out.println("");
         }
-  }
+    }
+}
