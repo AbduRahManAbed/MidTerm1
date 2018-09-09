c7bae211b37a7467e2f7b5d10fc977855be78535
[Abdu-rahman Abed] <[hanini1999@gmail.com]>
2018-09-09 18:06:55 -0400

    Factorial

--- src/math/problems/Factorial.java
+++ src/math/problems/Factorial.java
@@ -4,16 +4,17 @@ package math.problems;
  */
 public class Factorial {
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         int factorial = fact(3);
-        System.out.println("Factorial of 3 is: "+factorial);
+        System.out.println("Factorial of 3 is: " + factorial);
     }
+
     static int fact(int n) {
         int output;
-        if(n==1){
+        if (n == 1) {
             return 1;
         }
-        output = fact(n-1)* n;
+        output = fact(n - 1) * n;
         return output;
     }
 }
