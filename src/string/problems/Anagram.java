afaee3bc3a8e08db37f58598cca5520c21621c55
[Abdu-rahman Abed] <[hanini1999@gmail.com]>
2018-09-09 21:05:08 -0400

    Anagram

--- src/string/problems/Anagram.java
+++ src/string/problems/Anagram.java
@@ -1,12 +1,41 @@
 package string.problems;
 
+import java.util.Arrays;
+
 /**
  * Created by mrahman on 04/22/17.
  */
 public class Anagram {
+    private static String str1 = "ARMY";
+    private static String str2 = "MARY";
 
     public static void main(String[] args) {
-        //Write a Java Program to check if the two String are Anagram. Two String are called Anagram when there is
-        //same character but in different order.For example,"CAT" and "ACT", "ARMY" and "MARY".
+        if (isAnagram(str1, str2)) {
+            System.out.println(str2 + " is anagram of " + str1);
+            } else {
+            System.out.println("Is not anagram.");
+        }
     }
+
+    private static boolean isAnagram(String str1, String str2) {
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+        str1 = sortCharacters(str1);
+        str2 = sortCharacters(str2);
+
+        if (str1.equals(str2)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private static String sortCharacters(String str) {
+        char[] charArray = str.toLowerCase().toCharArray();
+        Arrays.sort(charArray);
+        return String.valueOf(charArray);
+    }
+
 }
+
