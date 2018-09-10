2b49d36fbd325d9196e02a1a63040e5189566143
[Abdu-rahman Abed] <[hanini1999@gmail.com]>
2018-09-09 21:16:24 -0400

    DuplicateWord

--- src/string/problems/DuplicateWord.java
+++ src/string/problems/DuplicateWord.java
@@ -1,18 +1,29 @@
 package string.problems;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
 /**
  * Created by mrahman on 04/22/17.
  */
 public class DuplicateWord {
 
     public static void main(String[] args) {
-        /*
-         * Write a java program to find the duplicate words and their number of occurrences in the string.
-         * Also Find the average length of the words.
-         */
+        NumberOfDuplicates("Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language");
+    }
 
-        String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
+    public static void NumberOfDuplicates(String sentence) {
+        String[] words = sentence.split(" ");
+        HashMap<String, Integer> Map;
+        Map = new LinkedHashMap<String, Integer>();
 
+        for (String word : words) {
+            Map.put(word,(Map.get(word) == null ? 1 : (Map.get(word) + 1)));
+        }
+        System.out.println(Map);
     }
 
 }
+
+
+
