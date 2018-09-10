0f5e261c7601cde48fc7c676676a312808e5985c
[Abdu-rahman Abed] <[hanini1999@gmail.com]>
2018-09-09 20:04:34 -0400

    Pattern

--- src/math/problems/Pattern.java
+++ src/math/problems/Pattern.java
@@ -3,10 +3,10 @@ package math.problems;
 import java.util.ArrayList;
 
 public class Pattern {
-	static int cntA = 1;
-	static int cntB = 10;
-	static int cntC = 0;
-	static int currentNum = 100;
+	private static int cntA = 1;
+	private static int cntB = 10;
+	private static int cntC = 0;
+	private static int currentNum = 100;
 
 	public static void main(String[] args) {
 		ArrayList<Integer> numbers = new ArrayList();
